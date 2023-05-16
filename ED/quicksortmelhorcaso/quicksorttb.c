dd373e6428704691ab4de79c3f0f477546124d3b
ErickBezerrar <erickbrtrindade@gmail.com>
2023-05-16 11:14:09 -0300

    txt

--- ED/quicksortmelhorcaso/quicksorttb.c
+++ ED/quicksortmelhorcaso/quicksorttb.c
@@ -1,60 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 
-int partition(int *v,int i,int f)
-{
-    int p=i;
-	int b=i+1;
-	int k,a;
-	for(k=i+1;k<=f;k++){
-		if(v[p]>v[k]){
-			a=v[k];
-			v[k]=v[b];
-			v[b]=a;
-			b++;
-		}
-	}
-	a=v[b-1];
-	v[b-1]=v[p];
-	v[p]=a;
-	return b-1;
-}
-void quick_sort(int *A,int i,int f)
-{
-	int p,m,aux;
-    if (i<f){
-			m=(i+f)/2;
-			aux=A[i];
-			A[i]=A[m];
-			A[m]=aux;
-            p=partition(A,i,f);
-			quick_sort(A,i,p-1);
-			quick_sort(A,p+1,f);
-    }
-}     
+int partition(int *v, int s, int e);
 
-int main()
-{
-  int i;
+void quick_sort(int *v, int s, int e) {
+    int p;
+    if(s < e) {
+        p = partition(v, s, e);
+        quick_sort(v, s, p-1);
+        quick_sort(v, p+1, e);
+    }
+}    
+
+int partition(int *v, int s, int e) {
+    int l = s;
+    int i, aux;
+    for(i=s; i < e; i++) {
+        if(v[i] < v[e]) {
+            aux = v[i];
+            v[i] = v[l];
+            v[l] = aux;
+            l = l + 1;
+        }
+    }
+    aux = v[e];
+    v[e] = v[l];
+    v[l] = aux;
+
+    return l;
+}
+
+int main(){
+    int i;
 	float tempo;
 	struct timeval a,b;
 	float v;
 	FILE *fp;
 	int n=100;
     fp = fopen ("quick_sort_melhor_caso.txt", "w");
-	while(n<=10000){
-	    int A[n];
+	
+    while(n<=10000){
+	    int v[n];
 
         gettimeofday(&b,NULL);
-         for(i=0;i<n;i++){
-		    A[i]=i;
+        for(i=0; i<n; i++){
+		    v[i]=i;
 		}
-        quick_sort(A,0,n-1);
+        quick_sort(v,0,n-1);
         gettimeofday(&a,NULL);
-	    tempo=(a.tv_sec+a.tv_usec*1e-6)-(b.tv_sec+b.tv_usec*1e-6);
+	    tempo =(a.tv_sec+a.tv_usec*1e-6)-(b.tv_sec+b.tv_usec*1e-6);
 		fprintf(fp,"%d %f\n",n,tempo*1e6);
-		n=n+100;
+		n = n+100;
 	}	
-	return 0;
+	
+    return 0;
 
 }
