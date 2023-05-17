1362efcaeae482f84d147428127dcbea98ef4d4d
ErickBezerrar <erickbrtrindade@gmail.com>
2023-05-17 15:25:27 -0300

    codes

--- ED/selectionsort/selectionsort.c
+++ ED/selectionsort/selectionsort.c
@@ -37,28 +37,20 @@ int main()
     FILE *fp;
     fp = fopen("selection_sort.txt", "w");
     
-    int num_execucoes = 10; // Define o número de execuções desejadas
-    
-    for (int execucao = 1; execucao <= num_execucoes; execucao++) {
-        while (n <= 1000) {
-            int A[n];
-            tempo = 0; // Reinicializa o tempo de execução para cada tamanho de vetor
-            for (j = 0; j < n; j++) {
-                gettimeofday(&b, NULL);
-                for (i = 0; i < n; i++) {
-                    A[i] = rand() % ((100 - 0 + 1) + 1);
-                }
-                selection_sort(A, n);
-                gettimeofday(&a, NULL);
-                tempo += (a.tv_sec + a.tv_usec * 1e-6) - (b.tv_sec + b.tv_usec * 1e-6);
+    while (n <= 2000) {
+        int A[n];
+        for (j = 0; j < n; j++) {
+            gettimeofday(&b, NULL);
+            for (i = 0; i < n; i++) {
+                A[i] = rand() % ((100 - 0 + 1) + 1);
             }
-            fprintf(fp, "%d %f\n", n, (tempo * 1e6) / n); // Calcula a média do tempo de execução por iteração
-            n = n + 100;
+            selection_sort(A, n);
+            gettimeofday(&a, NULL);
+            tempo += (a.tv_sec + a.tv_usec * 1e-6) - (b.tv_sec + b.tv_usec * 1e-6);
         }
-        n = 1;
-    }
-    
-    fclose(fp);
-    
+        fprintf(fp, "%d %f\n", n, (tempo * 1e6) / n);
+        tempo = 0;           
+        n = n + 100;
+    }    
     return 0;
 }
